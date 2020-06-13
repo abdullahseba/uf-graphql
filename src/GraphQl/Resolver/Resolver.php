d532afdff829655e47cb8157c55915b433cfd8a9
abdullahseba <abdullah.seba@outlook.com>
2020-06-14 00:23:35 +0100

    Update Resolver.php

--- src/GraphQl/Resolver/Resolver.php
+++ src/GraphQl/Resolver/Resolver.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * API Sprinkle
  *
@@ -17,4 +18,30 @@ use GraphQL\Type\Definition\Type;
  */
 class Resolver
 {
+    public static  $map = [];
+    public static function renameKeys($array, $map, $flipMap = false)
+    {
+        $map = $flipMap ? array_flip($map) : $map;
+        // error_log(print_r($array, true));
+
+        $new_array = array();
+        foreach ($array as $item => $value) {
+            // break;
+            if (is_array($array[$item])) {
+
+                $name = isset($map[$item]) ? $map[$item] : $item;
+
+                $new_array[$name] =  self::renameKeys($array[$item], $map, $flipMap);
+            } else {
+                // error_log(print_r($item, true));
+
+                $name = isset($map[$item]) ? $map[$item] : $item;
+                // error_log(print_r($item, true));
+
+                $new_array[$name] = $value;
+            }
+        }
+
+        return $new_array;
+    }
 }
